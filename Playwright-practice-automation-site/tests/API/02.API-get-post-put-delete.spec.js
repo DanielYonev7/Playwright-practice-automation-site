34500d9b1a1eae885b7f34b610b66ef6f2986fc0
Daniel <daniel.yonev@gmail.com>
2025-12-22 11:30:02 +0200

    new api tests

--- Playwright-practice-automation-site/tests/API/02.API-get-post-put-delete.spec.js
+++ Playwright-practice-automation-site/tests/API/02.API-get-post-put-delete.spec.js
@@ -1,12 +1,13 @@
+
 import {test, expect} from '../../Application/baseFixture';
 
-test.describe("API - Get, Post, Put, Delete", ()=>{
+test.describe("API - Get, Post, Put, Delete - Pets", ()=>{
 
     test.beforeEach(async ({page})=>{
         await page.goto('https://petstore.swagger.io/');
     })
 
-    test("01. API - GET status", async ({page, request})=>{
+    test("01. API - GET status", async ({page})=>{
 
         const response = await page.request.get('https://petstore.swagger.io/v2/pet/1');
         const responseJson = await response.json();
@@ -17,5 +18,41 @@ test.describe("API - Get, Post, Put, Delete", ()=>{
         const responseBody = await response.text();
         expect(responseBody).toContain('"id":1')
         expect(responseBody).toContain('"name":"dog"')
+    })
+
+    test("02. API - PUT status", async ({page})=>{
+
+        const response = await page.request.put('https://petstore.swagger.io/v2/pet/', {
+            data: {
+                id: 353,
+                category: { id: 0, name: 'sushi' },
+                name: 'testName',
+                photoUrls: ['string'],
+                tags: [{ id: 0, name: 'string' }],
+                status: 'unavailable'
+            },
+
+            headers: {'Content-Type': 'application/json'}
+        })
+
+        const responseBody = await response.json();
+
+        expect(response.status()).toBe(200);
+        expect(responseBody.id).toBe(353);
+        expect(responseBody.name).toBe('testName')
+    })
+
+
+    test.only("03. API - GET - findbystatus", async ({page})=>{
+        const response = await page.request.get('https://petstore.swagger.io/v2/pet/findByStatus',  {
+            //checking endpoing with parameter 'status' sold
+            params: {status: 'sold'}
+        });
+
+        const responseBody = await response.json()
+        console.log(responseBody)
+
+        expect(response.status()).toBe(200);
+        console.log('The number of elements that are sold is:',responseBody.length)
     })
 })
